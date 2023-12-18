083c34bbdfdde0d389cf412123283b0c7fc7a1f8
Kiraogi <golokron3@yandex.ru>
2023-12-18 21:32:19 +0300

    Ver 0.04

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -15,6 +16,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
         # Назначение цвета фона.
         self.bg_color = (230, 230, 230)
 
@@ -27,6 +30,7 @@ class AlienInvasion:
                     sys.exit()
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
