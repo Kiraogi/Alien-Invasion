1db2ee5224f26d2b116a8d275b6c3e0dbd673a77
Kiraogi <golokron3@yandex.ru>
2023-12-20 22:09:37 +0300

    Ver 0.09

--- settings.py
+++ settings.py
@@ -7,6 +7,12 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        # Параметры снаряда.
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
 
         # Настройки корабля.
         self.ship_speed = 1.5
