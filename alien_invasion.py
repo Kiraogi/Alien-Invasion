745d972b603fc681f3ac252dabd0b8abd2bd1c99
Kiraogi <golokron3@yandex.ru>
2023-12-21 22:03:57 +0300

    Ver 0.11

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,6 +90,19 @@ class AlienInvasion:
         """Создание флота вторжения."""
         # Создание пришельца.
         alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        # Создание пришельца и размещение его в ряду.
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Создание пришельца и размещение его в ряду."""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         self.aliens.add(alien)
 
     def _update_screen(self):
